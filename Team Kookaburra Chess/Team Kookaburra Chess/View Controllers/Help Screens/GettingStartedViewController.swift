6799ae5f7aa938dc571521e86bb4a41916157b05
Blake Erquiaga <cbcerquiaga@me.com>
2019-07-05 10:25:14 -0400

    Added View Controllers and Scenes for Expanded How-To
    #17 I haven't done any design, added buttons/segues, or written any text yet, but the framework is there to start working on it now.

---
+++ Team Kookaburra Chess/Team Kookaburra Chess/View Controllers/Help Screens/GettingStartedViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GettingStartedViewController.swift
+//  Team Kookaburra Chess
+//
+//  Created by Christopher Blake Cassell Erquiaga on 7/5/19.
+//  Copyright © 2019 Christopher Blake Cassell Erquiaga. All rights reserved.
+//
+
+import Foundation
